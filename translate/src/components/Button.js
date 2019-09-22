575b72d84364532110a6813b23d03678d409fbc3
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2019-09-22 11:58:04 +0100

    working with contexts

---
+++ translate/src/components/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+  static contextType = LanguageContext;
+
+  renderSubmit = ({ language }) => {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {this.renderSubmit}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    )
+  }
+}
+
+export default Button;
